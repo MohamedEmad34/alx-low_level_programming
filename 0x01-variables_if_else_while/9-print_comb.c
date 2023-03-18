ed2ee1fdc9feb2dc344ffc18d5c4df9a01efd278
Your Name <you@gmail.com>
2023-03-18 00:58:47 -0700

    modifie

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int low;
+
+        for (low = '0'; low <= '9'; low++)
+	{	
+        putchar(low);
+	if(low != '9')
+	{
+	putchar(',');
+	putchar(' ');
+	}	
+	}
+        putchar('\n');
+	return (0);
+}
