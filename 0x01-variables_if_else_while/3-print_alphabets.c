bb7b0ea1368b6290d14e8e8e585ed4d1c8b96d66
Your Name <you@gmail.com>
2023-03-17 23:21:05 -0700

    modified

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,10 @@ int main(void)
 {
 	char low;
 
-	for (low = 'a';low <= 'z'; low++)
-	putchar(low);
-	
-	for (low = 'A';low <= 'Z'; low++)
-	putchar(low);
+	for (low = 'a'; low <= 'z'; low++)
+	putchar(low);	
+	for (low = 'A'; low <= 'Z'; low++)
+	Putchar(low);
 	putchar('\n');
 	return (0);
-}	
+}
