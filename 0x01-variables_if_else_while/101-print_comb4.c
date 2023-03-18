4a71ab0095d20768b41cdeae97aadce46c343809
Your Name <you@gmail.com>
2023-03-18 02:27:43 -0700

    modified

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,17 +10,21 @@ int main(void)
         int low;
 	int high;
 	int size;
-        for (low = '0'; low < '8'; low++)
+        for (low = '0'; low < '8'; low++6)
+	{
         for (high = low + 1; high  < '9'; low++)
+	{
 	for (size = high + 1; size <= '9'; size++)
 	{
 	putchar(low);
 	putchar(high);
-	putchar(size;
+	putchar(size);
 	if (low = '7' && high == '8' && size == '9')
 	continue;
 	putchar(',');
-	putchar(' ');	
+	putchar(' ');
+	}
+	}
 	}	
         putchar('\n');
         return (0);
