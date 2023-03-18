64251d31b13d196dcf7aa2bdd442ecb6c693c584
Your Name <you@gmail.com>
2023-03-18 01:18:06 -0700

    file 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char low;
+	char high;
+
+        for (low = '0'; low < '9'; low++)
+	{
+	for (high = low+1 ; high <= '9'; high++)
+	{
+	putchar(low);
+	putchar(high);
+	if (low == '8' && high == '9')
+	continue;	
+	putchar(',');
+	putchar(' ');	
+	}	
+	}
+        putchar('\n');
+	return (0);
+}
