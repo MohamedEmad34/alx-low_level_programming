a3ad3f39db98e233cc1039c41cecf4eff47a143e
Your Name <you@gmail.com>
2023-03-18 04:42:02 -0700

    modified

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,10 +5,8 @@
 
 /**
  * main function
- * is used for find last digit
- * chek if is greater or less than and equal zero
- *
- * Return Always:0.   */
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -17,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if ((n % 10) > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);	
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 	}
-	else 
+	else
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
-	}	
+	}
 	return (0);
 }
