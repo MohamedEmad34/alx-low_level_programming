5970b729e5b6cf6b1d67b8f15b3eccdb53fe43d2
Your Name <you@gmail.com>
2023-03-18 02:52:32 -0700

    file 102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int p,q;
+
+        for (p = '0'; p  <= '98'; p++)
+	{
+        for (q = p + 1; q <= '99'; q++)
+	{
+	putchar((p / 10) + '0');
+	putchar((p % 10) + '0');
+	putchar(' ');
+	putchar((q / 10) + '0');
+	putchar((q % 10) + '0');
+	}
+	}	
+	putchar('\n');
+        return (0);
+}
