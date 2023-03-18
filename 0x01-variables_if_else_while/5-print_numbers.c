141c2f407d223cb1cb6aebdabb0888f26ae00d9c
Your Name <you@gmail.com>
2023-03-17 23:40:48 -0700

    5 file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry number of single digit
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        int low;
+
+        for (low = 0; low <= 9; low++)
+        printf("%d", low);
+	printf("\n");
+	
+	return (0);
+}
