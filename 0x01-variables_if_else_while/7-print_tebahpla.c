fbea3f1c5b95025cb627c3a6a58d5ba9b9001321
Your Name <you@gmail.com>
2023-03-18 00:02:08 -0700

    file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main prints the alphabet
+ * Return: Always (success)
+ */
+int main(void)
+{
+        char c;
+
+        for ('z' >= c; c >= 'a'; c--)
+        putchar(c);
+	putchar('\n');
+	return (0);
+}
