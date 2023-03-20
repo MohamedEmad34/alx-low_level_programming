511c787d72af41eea51bbc34036d55554daba8f7
Your Name <you@gmail.com>
2023-03-20 06:28:20 -0700

    mod

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 
 /**
  * main - Enhtry point
- * Return : 1
+ * Return: 1
  */
 int main(void)
 {
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
