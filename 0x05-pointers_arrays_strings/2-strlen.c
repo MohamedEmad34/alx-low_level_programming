d81aca1a5c4e02168052e8ba9f9020df59e254c6
MohamedEmad34 <me632453@gmail.com>
2023-03-27 08:40:42 -0700

    2 file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Takes a pointer to an char and return length
+ *
+ * @str: An string
+ *
+ * Return: nothing.
+ */
+int _strlen(char *str)
+{
+	return (strlen(*str));
+}
+
