45acf1ca861c3f5bc11b2da8e4cb8c60df34c89b
MohamedEmad34 <me632453@gmail.com>
2023-04-09 14:06:17 -0700

    modified

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: the value of the pointer
+ * @to: the value to convert it
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
