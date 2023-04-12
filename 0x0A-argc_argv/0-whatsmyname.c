3b83a3f81ae21d4e217b36c2f9955d230b731822
MohamedEmad34 <me632453@gmail.com>
2023-04-12 00:31:54 -0700

    mod

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument
+ * @argv: argument
+ * Return: Always zero
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
