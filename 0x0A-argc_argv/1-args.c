47f196f0a8cb17d6cc2a483be3f19cf0e259f789
MohamedEmad34 <me632453@gmail.com>
2023-04-12 00:38:14 -0700

    mod

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: argument
+ * @argv: argument
+ * Return: Always zero
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
