9b78827dad771def5fe87dbc2cbea66ed0963e1c
MohamedEmad34 <me632453@gmail.com>
2023-04-15 05:01:39 -0700

    mod

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: Entry - point
+ * Return: nothing
+ */
+ 
+void print_alphabet_x10(void)
+{
+	char s;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+        for (s = 'a'; s <= 'z'; s++)
+                _putchar(s);
+        _putchar('\n');
+	}
+}	
