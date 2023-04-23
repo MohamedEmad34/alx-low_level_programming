f4229caa1cbabeea4fe299b33977587a76521df6
MohamedEmad34 <me632453@gmail.com>
2023-04-23 04:16:23 -0700

    mod

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _print_rev_recursion - Entry point
+ * @s: input
+ * Return: nothing
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
