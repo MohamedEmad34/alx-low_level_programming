d51dab95a782aeed9cb674b5ca718ab879679d97
MohamedEmad34 <me632453@gmail.com>
2023-04-23 04:55:32 -0700

    mod

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - Entry point
+ * @x: input
+ * @y: input
+ * Return: integer value
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+	return (1);
+	else if (y < 0)
+		return (-1);
+	return x * _pow_recursion(x, y - 1);
+}
