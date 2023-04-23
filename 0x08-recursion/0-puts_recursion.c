8b3345d1619e26b6b9b0f3a339ca55de2b52ef31
MohamedEmad34 <me632453@gmail.com>
2023-04-23 03:43:47 -0700

    mod

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,14 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _puts_recursion - recurse function
+ * @s: input
+ * Return: nothing
+ */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0'){
+	if (*s == '\0')
+	{
 		printf("\n");
 	      return;
 
